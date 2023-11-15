849f1356e392f470834321282f5decdedbb54811
Tereza Smolova <tereza.smolova@gmail.com>
2023-11-15 16:25:56 +0100

    Working in progress

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -3,52 +3,83 @@ import React from 'react';
 import './style.scss';
 import iconClose from './img/icon-close.svg'
 import iconNav from './img/icon-nav.svg'
+import { useState } from "react";
 
-export const Navigation = () => (
-  <>
-    {/* Mobile-nav */}
-    <div className="mobile-nav">
+export const Navigation = () => {
+  const [mobileNav, setMobileNav] = useState(true)
+
+  const handleClick = () => {
+    setMobileNav(!mobileNav 
+      ? null 
+      : (
+        <div className="mobile-nav">
+      
+          <img onClick={handleClick} src={iconNav} alt="ikona hamburger menu" />
     
+          <nav className="mobile-nav__nav">
+            <a className="mobile-nav__icon-close" to="/">
+              <img src={iconClose} alt="ikona zavřít" />
+            </a>
+            <ul className="mobile-nav__list">
+              <li className="mobile-nav__item">
+                <Link className="mobile-nav__link" to="/">Domů</Link>
+              </li>
+              <li className="mobile-nav__item">
+                <Link className="mobile-nav__link" to="/hedvika">Hedvika</Link>
+              </li>
+              <li className="mobile-nav__item">
+                <Link className="mobile-nav__link" to="/tynka">Týnka</Link>
+              </li>
+              <li className="mobile-nav__item">
+                <Link className="mobile-nav__link" to="/terka">Terka</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      ))
+  }
 
-      <a className="mobile-nav__icon-close" to="index.html">
-        <img src={iconClose} alt="ikona zavřít" />
-      </a>
-
-      <a className="mobile-nav__icon-close" to="index.html">
-        <img src={iconNav} alt="ikona zavřít" />
-      </a>
-
-      <nav className="mobile-nav__nav">
-        <ul className="mobile-nav__list">
-          <li className="mobile-nav__item">
-            <Link className="mobile-nav__link" to="/">Domů</Link>
+  return(
+    <>
+      {/* <div className="mobile-nav">
+      
+        <img onClick={handleClick} src={iconNav} alt="ikona hamburger menu" />
+  
+        <nav className="mobile-nav__nav">
+          <a className="mobile-nav__icon-close" to="/">
+            <img src={iconClose} alt="ikona zavřít" />
+          </a>
+          <ul className="mobile-nav__list">
+            <li className="mobile-nav__item">
+              <Link className="mobile-nav__link" to="/">Domů</Link>
+            </li>
+            <li className="mobile-nav__item">
+              <Link className="mobile-nav__link" to="/hedvika">Hedvika</Link>
+            </li>
+            <li className="mobile-nav__item">
+              <Link className="mobile-nav__link" to="/tynka">Týnka</Link>
+            </li>
+            <li className="mobile-nav__item">
+              <Link className="mobile-nav__link" to="/terka">Terka</Link>
+            </li>
+          </ul>
+        </nav>
+      </div> */}
+  
+      {/* Navigation */}
+      <nav className="navigation">
+        <ul className="navigation__list">
+          <li className="navigation__item">
+            <Link className="navigation__link" to="/hedvika">Hedvika</Link>
           </li>
-          <li className="mobile-nav__item">
-            <Link className="mobile-nav__link" to="/hedvika">Hedvika</Link>
+          <li className="navigation__item">
+            <Link className="navigation__link" to="/tynka">Týnka</Link>
           </li>
-          <li className="mobile-nav__item">
-            <Link className="mobile-nav__link" to="/tynka">Týnka</Link>
-          </li>
-          <li className="mobile-nav__item">
-            <Link className="mobile-nav__link" to="/terka">Terka</Link>
+          <li className="navigation__item">
+            <Link className="navigation__link" to="/terka">Terka</Link>
           </li>
         </ul>
       </nav>
-    </div>
-
-    {/* Navigation */}
-    <nav className="navigation">
-      <ul className="navigation__list">
-        <li className="navigation__item">
-          <Link className="navigation__link" to="/hedvika">Hedvika</Link>
-        </li>
-        <li className="navigation__item">
-          <Link className="navigation__link" to="/tynka">Týnka</Link>
-        </li>
-        <li className="navigation__item">
-          <Link className="navigation__link" to="/terka">Terka</Link>
-        </li>
-      </ul>
-    </nav>
-  </>
-);
+    </>
+  )
+};
