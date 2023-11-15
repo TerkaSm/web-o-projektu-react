7433b4b9ea0fae2767b23fd2ceb232a27457591d
Tereza Smolova <tereza.smolova@gmail.com>
2023-11-15 14:54:03 +0100

    Working in progress

--- src/lockets.js
+++ src/lockets.js
@@ -1,38 +1,42 @@
-// import imgSrcHedvika from './assets/hedvika_reichlova.jpg'
-// import imgScrTynka from './assets/kristyna_jozifova.jpg'
+import imgSrcHedvika from './assets/hedvika_reichlova.jpg'
+import imgSrcTynka from './assets/kristyna_jozifova.jpg'
+import imgSrcTerka from './assets/tereza_smolova.jpg'
+import imgSrcPanda from './assets/panda.svg'
+import imgSrcKocka from './assets/kocka.svg'
+import imgSrcTygr from './assets/tygr.svg'
 
 export const LOCKETS = { 
      hedvika: {
         id: "hedvika",
-        imgPerson: "./assets/hedvika_reichlova.jpg",
+        imgPerson: imgSrcHedvika,
         namePerson: "Hedvika Reichlová",
         gitHubLink: "https://github.com/DezesTheOne",
         textPerson: "Ahoj! Jsem Hedvika a na stará kolena jsem se rozhodla změnit významně kariéru. Přihlásila jsem se na Digitální akademii od Czechitas a chci se stát frontend developerem. Baví mě spoustu věcí, programování, animování, malování, fotografování, počítačové hry a do toho se snažím lézt a zbavovat se strachu z výšek. Jsem věčný hledač technických vychytávek a objevitel toho jak věci fungují. Doufám, že náš projekt bude dělat radost dětem i rodičům.",
         nameAnimal: "Panda Olinka",
         textAnimal: "Hru jsme vytvořily jako projekt na Digitální akademii 2023. Hledala jsem něco co má psychologický přesah. Narazila jsem na hru od Sesame Street a protože nemá českou verzi, rozhodly jsme se jí přetvořit po svém. Moje zvířátko je panda Olinka. Pojmenovala jsem jí po své neteři. Ve hře mám na starost hlavně animace a zvuky. Snad naše hra bude děti bavit a přinese jim dovednost, která je bude provázet po zbytek dětství a vlastně i dospělosti.",
         altAnimal: "panda",
-        imgAnimal: "assets/panda.svg",
+        imgAnimal: imgSrcPanda,
     },
     tynka: {
         id: "tynka",
-        imgPerson: "./assets/kristyna_jozifova.jpg",
+        imgPerson: imgSrcTynka,
         namePerson: "Kristýna Jozífová",
         gitHubLink: "https://github.com/Kristyn-a",
         textPerson: "Ahoj, jmenuji se Kristýna a v mém dosavadním pracovním životě se žádné IT do dnešních dnů neobjevovalo. Toto odvětví mě zaujalo svojí rozmanitostí a vstřícností. Před pár lety jsem si vyzkoušela první kurz a byla to zábava se záplavou infomací, které nikdy nekončí. Ráda bych se posouvala a udělala progres v tomto odvětví.",
         nameAnimal: "Kočička Tonička",
         textAnimal: "Kočička Tonička provede dítě věčerním usínáním. Ukáže mu jak na to, aby se nebálo a byla to běžná situace. Pro větší přitažlivost hry bude kočička Tonička namluvená a informace bude předávat přes audio a návodné obrázky.",
         altAnimal: "kočka",
-        imgAnimal: "assets/kocka.svg",
+        imgAnimal: imgSrcKocka,
     },
     terka: {
         id: "terka",
-        imgPerson: "assets/tereza_smolova.jpg",
+        imgPerson: imgSrcTerka,
         namePerson: "Tereza Smolová",
         gitHubLink: "https://github.com/TerkaSm",
         textPerson: "Ahoj jmenuju se Tereza a mojí novou vášní je frontend. Proto jsem se přihlásila na Digitální akademii Web od Czechitas a tenhle web je info-webem o našem závěrečném projektu. Jsem mámou tříletého kluka a tak doufám že náš projekt bude použitelný, užitečný a pomůže rodičům při zvládání náročných dětských situací a emocí.",
         nameAnimal: "Tygřík Kubík",
         textAnimal: "Ve hře hlasově ztvárňuji tygříka Kubíka, který dítě provede situací kdy mu spadne věž z kostek. Kubík dítěti ukáže jak se lze v takové situaci zachovat a vyřešit ji. Nejdříve se zastaví, s pomocí dítěte se párkrát nadechne, potom se zamyslí a nabídne možnosti řešení. Výběr je na dítěti. Nakonec si vše projdou ještě jednou.",
         altAnimal: "tygr",
-        imgAnimal: "assets/tygr.svg",
+        imgAnimal: imgSrcTygr,
     },
 }
