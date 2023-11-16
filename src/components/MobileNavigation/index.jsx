013579f9ffced921785177ad3a07fd855a6e31b5
Tereza Smolova <tereza.smolova@gmail.com>
2023-11-16 16:21:04 +0100

    Mobile nav

--- src/components/MobileNavigation/index.jsx
+++ src/components/MobileNavigation/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './style.scss';
-import iconClose from './img/icon-close.svg'
-import iconNav from './img/icon-nav.svg'
 import { useState } from "react";
 
 export const MobileNavigation = () => {
@@ -14,28 +12,33 @@ export const MobileNavigation = () => {
 
   return(
     <div className="mobile-nav">
-      
-        <img onClick={toggleMobileNav} src={iconNav} alt="ikona hamburger menu" />
 
-        <nav className="mobile-nav__nav">
-        <a className="mobile-nav__icon-close" to="/">
-            <img src={iconClose} alt="ikona zavřít" />
-        </a>
-        <ul className="mobile-nav__list">
-            <li className="mobile-nav__item">
-            <Link className="mobile-nav__link" to="/">Domů</Link>
-            </li>
-            <li className="mobile-nav__item">
-            <Link className="mobile-nav__link" to="/hedvika">Hedvika</Link>
-            </li>
-            <li className="mobile-nav__item">
-            <Link className="mobile-nav__link" to="/tynka">Týnka</Link>
-            </li>
-            <li className="mobile-nav__item">
-            <Link className="mobile-nav__link" to="/terka">Terka</Link>
-            </li>
-        </ul>
-        </nav>
+        <div
+            onClick={toggleMobileNav}
+            className={`mobile-nav__icon ${
+                isOpenMobileNav ? 'mobile-nav__icon--close' : 'mobile-nav__icon--open'
+            }`}>
+        </div>
+
+        {isOpenMobileNav 
+        ? null
+        :   <nav className="mobile-nav__nav">
+                <ul className="mobile-nav__list">
+                    <li className="mobile-nav__item">
+                    <Link className="mobile-nav__link" to="/">Domů</Link>
+                    </li>
+                    <li className="mobile-nav__item">
+                    <Link className="mobile-nav__link" to="/hedvika">Hedvika</Link>
+                    </li>
+                    <li className="mobile-nav__item">
+                    <Link className="mobile-nav__link" to="/tynka">Týnka</Link>
+                    </li>
+                    <li className="mobile-nav__item">
+                    <Link className="mobile-nav__link" to="/terka">Terka</Link>
+                    </li>
+                </ul>
+            </nav>
+        }
     </div>
   )
 };
